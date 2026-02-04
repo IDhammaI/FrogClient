54594a32c874f4751954a08b051f143fb5084d46
Dhamma <1518470138@qq.com>
2026-02-04 13:09:59 +0800

    Item显示字体添加平滑字体

--- src/main/java/dev/idhammai/mod/modules/impl/client/hud/ItemCounterHudModule.java
+++ src/main/java/dev/idhammai/mod/modules/impl/client/hud/ItemCounterHudModule.java
@@ -1,7 +1,9 @@
 package dev.idhammai.mod.modules.impl.client.hud;
 
 import dev.idhammai.api.utils.player.InventoryUtil;
+import dev.idhammai.core.impl.FontManager;
 import dev.idhammai.mod.modules.HudModule;
+import dev.idhammai.mod.modules.settings.impl.BooleanSetting;
 import java.util.function.IntSupplier;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.item.Item;
@@ -9,6 +11,8 @@ import net.minecraft.item.ItemConvertible;
 import net.minecraft.item.ItemStack;
 
 public class ItemCounterHudModule extends HudModule {
+    private final BooleanSetting font = this.add(new BooleanSetting("Font", true).setParent());
+    private final BooleanSetting shadow = this.add(new BooleanSetting("Shadow", true, () -> this.font.isOpen()));
     private final ItemStack stack;
     private final IntSupplier countSupplier;
 
@@ -34,6 +38,16 @@ public class ItemCounterHudModule extends HudModule {
         int py = this.getHudY();
         this.setHudBounds(px, py, 16, 16);
         drawContext.drawItem(this.stack, px, py);
-        drawContext.drawItemInSlot(ItemCounterHudModule.mc.textRenderer, this.stack, px, py);
+        if (this.font.getValue()) {
+            String s = String.valueOf(count);
+            int tx = px + 16 - (int)Math.ceil(FontManager.ui.getWidth(s));
+            int ty = py + 16 - (int)Math.ceil(FontManager.ui.getFontHeight());
+            drawContext.getMatrices().push();
+            drawContext.getMatrices().translate(0.0f, 0.0f, 200.0f);
+            FontManager.ui.drawString(drawContext.getMatrices(), s, (double)(tx + 1), (double)(ty + 1), -1, this.shadow.getValue());
+            drawContext.getMatrices().pop();
+        } else {
+            drawContext.drawItemInSlot(ItemCounterHudModule.mc.textRenderer, this.stack, px, py);
+        }
     }
 }
